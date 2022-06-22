eaf1c92e177589b1f3accbdb9b4c44bb80c5866e
MikeyBadi <mihaibadia.1999@gmail.com>
2022-06-22 18:15:31 +0200

    finished

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-});
+
+    $datas =[
+        'welcome'=> 'Welcome to my new and awesome Laravel first project',
+        'comps'=>['home'=>'Home','about'=>'About','projects'=>'Projects']
+    ];
+
+    return view('home',$datas);
+})->name('home');
+
+Route::get('/about', function () {
+    $datas =[
+        'about'=> 'This is what laravel looks like',
+        'img'=> 'https://images.ctfassets.net/rporu91m20dc/4KrCV1uQj6CvFqZIYRoQuG/c92e4dcf285e1424bf063ea375baa79a/1_Hell_on_Earth.jpg'
+    ];
+    return view('about',$datas);
+})->name('about');
